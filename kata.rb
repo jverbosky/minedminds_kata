0de976307d62c65b0a6a1a813614599e55dd1611
jverbosky <jverbosk@gmail.com>
2017-01-13 12:06:42 -0500

    first push of kata.rb

---
+++ kata.rb
@@ -0,0 +1,13 @@
+# Print 1 ten times to the screen
+puts "Print using .times method:"
+10.times { puts 1 }
+puts "\n"
+
+# Print 1 - 10 to the screen
+puts "Print 1 - 10 using while loop:"
+n = 1
+while n < 11
+	puts n
+	n += 1
+end
+
