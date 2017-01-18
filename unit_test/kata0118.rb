704e4e2ba3b7ce0a31885e0b059ffa053e331427
jverbosky <jverbosk@gmail.com>
2017-01-18 14:07:55 -0500

    paired with Max on test

--- unit_test/kata0118.rb
+++ unit_test/kata0118.rb
@@ -3,7 +3,7 @@
 # Unit Test example
 # Write code to test and make sure your code does what you say it does
 
-=begin
+
 
 # A method will always return the last item
 # So to return 1, can use this:
@@ -33,17 +33,16 @@ def minedmindskata(num)
   end
 end
 
-=end
+# # Need to call ("values") or return ("return values")
+# # Using "print values" or "puts values" will error on test
+# def minedmindskata()
+#   values = []
+#   (1..100).each { |num| values.push(num) }
+#   values.map! { |value| (value % 3 != 0 || value % 5 != 0) ? value : "Mined Minds" }
+#   values.map! { |value| value % 3 != 0 ? value : "Mined" }
+#   values.map! { |value| value % 5 != 0 ? value : "Minds" }
+#   values
+# end
 
-# Need to call ("values") or return ("return values")
-# Using "print values" or "puts values" will error on test
-def minedmindskata()
-  values = []
-  (1..100).each { |num| values.push(num) }
-  values.map! { |value| (value % 3 != 0 || value % 5 != 0) ? value : "Mined Minds" }
-  values.map! { |value| value % 3 != 0 ? value : "Mined" }
-  values.map! { |value| value % 5 != 0 ? value : "Minds" }
-  values
-end
+# minedmindskata()
 
-minedmindskata()
--- unit_test/test_kata0118.rb
+++ unit_test/test_kata0118.rb
@@ -13,8 +13,6 @@ require_relative "kata0118.rb"
 # Class to create a new instance for test (?)
 class TestMinedMindsKata < Minitest::Test
 
-=begin
-
 	# Note: method name has to begin with "test"
 	def test_1_returns_1
 		assert_equal(1,1)
@@ -44,12 +42,13 @@ class TestMinedMindsKata < Minitest::Test
 		assert_equal("Mined Minds",minedmindskata(15))
 	end
 
-=end
 
-	# Can use arrays to test ranges
-	def test_function_returns_array
-		assert_equal([1, 2, "Mined", 4, "Minds", "Mined", 7, 8, "Mined", "Minds", 11, "Mined", 13, 14, "Mined Minds", 16, 17, "Mined", 19, "Minds", "Mined", 22, 23, "Mined", "Minds", 26, "Mined", 28, 29, "Mined Minds", 31, 32, "Mined", 34, "Minds", "Mined", 37, 38, "Mined", "Minds", 41, "Mined", 43, 44, "Mined Minds", 46, 47, "Mined", 49, "Minds", "Mined", 52, 53, "Mined", "Minds", 56, "Mined", 58, 59, "Mined Minds", 61, 62, "Mined", 64, "Minds", "Mined", 67, 68, "Mined", "Minds", 71, "Mined", 73, 74, "Mined Minds", 76, 77, "Mined", 79, "Minds", "Mined", 82, 83, "Mined", "Minds", 86, "Mined", 88, 89, "Mined Minds", 91, 92, "Mined", 94, "Minds", "Mined", 97, 98, "Mined", "Minds"],minedmindskata())
-	end
+
+	# # Can use arrays to test ranges
+	# def test_function_returns_array
+	# 	assert_equal([1, 2, "Mined", 4, "Minds", "Mined", 7, 8, "Mined", "Minds", 11, "Mined", 13, 14, "Mined Minds", 16, 17, "Mined", 19, "Minds", "Mined", 22, 23, "Mined", "Minds", 26, "Mined", 28, 29, "Mined Minds", 31, 32, "Mined", 34, "Minds", "Mined", 37, 38, "Mined", "Minds", 41, "Mined", 43, 44, "Mined Minds", 46, 47, "Mined", 49, "Minds", "Mined", 52, 53, "Mined", "Minds", 56, "Mined", 58, 59, "Mined Minds", 61, 62, "Mined", 64, "Minds", "Mined", 67, 68, "Mined", "Minds", 71, "Mined", 73, 74, "Mined Minds", 76, 77, "Mined", 79, "Minds", "Mined", 82, 83, "Mined", "Minds", 86, "Mined", 88, 89, "Mined Minds", 91, 92, "Mined", 94, "Minds", "Mined", 97, 98, "Mined", "Minds"],minedmindskata())
+	# end
+
 
 end
 
