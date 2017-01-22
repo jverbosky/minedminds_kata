cc56bcec2a377e2d7f9bced8089a023b0fe57159
jverbosk <jverbosk@gmail.com>
2017-01-22 16:11:57 -0500

    cleaned up tips a bit

--- zoom/2017_01_22_study_tips.rb
+++ zoom/2017_01_22_study_tips.rb
@@ -2,25 +2,30 @@
 
 # First line of every test that Amanda writes begins with:
 # results = "something"
+# For example:
 
-# Every test needs to have an assert statement, or it is of no value
+def test_1_function_returns_array
+  results = create_array() # results to test against
+  assert_equal(Array,results.class)
+end
+
+# Every test *needs* to have an assert statement, or it is of no value
 
 # The first thing that we pass in is what we are expecting
 # The second thing is what we are running
 
 # It's always good to begin a function name with a verb, since it's doing something
+# And try to avoid using the same name for each variable and function
+# Helps you know what you are referring to, since it avoids ambiguity
 # For example, don't do this:
 
 def something()  # function name is "something"
   something = [] # so is the variable name for the array = very ambiguous
 end
 
-# Try to avoid using the same name for each variable and function
-# Helps you know what you are referring to, since it avoids ambiguity
-
 # Always run the test before you write the code
 # It should fail at that point
-# If you *don't* get a failure at this point, that should tell you something
+# If you *don't* get a failure at this point, that should tell you something isn't right
 
 # At the end of the function use:  return "something"
 # For example:
@@ -33,14 +38,15 @@ def create_array()
   return something # don't forget to return the result
 end
 
-# Don't have to use the word "return" before "something", but makes it clearer
-# what is being
+# Using the word "return" before "something" isn't required,
+# but does help clarify what is being returned
 
 # When developing a test, consider that you may not be able to see the output of a function
-# With that in mind, what would you need to see (test) to make sure
+# With that in mind...
+# When writing a test, consider what would you need to see (test) to make sure
 # that the function is working as you expect
 
-# Very common mistake is to increment within the else statement
+# A very common mistake is to increment within the else statement
 # For example:
 
 def create_array()
@@ -57,3 +63,5 @@ def create_array()
   end
   return something
 end
+
+# 2017-01-22 - jv
