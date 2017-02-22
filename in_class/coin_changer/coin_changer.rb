b1065b90fd7f902174e7da91f60502b85a1f9f4a
jverbosky <jverbosk@gmail.com>
2017-02-22 12:17:56 -0500

    refactored to use .each loop

--- in_class/coin_changer/coin_changer.rb
+++ in_class/coin_changer/coin_changer.rb
@@ -1,22 +1,14 @@
 def get_change(amount)
   change = {}
   coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
-  if amount >= coins[:quarter]
-    number_coins = amount / coins[:quarter]
-    change[:quarter] = number_coins
-    amount -= coins[:quarter] * number_coins
+
+  coins.each do |coin, value|
+    if amount >= value
+      number_coins = amount / value
+      change[coin] = number_coins
+      amount -= value * number_coins
+    end
   end
-  if amount >= coins[:dime]
-    number_coins = amount / coins[:dime]
-    change[:dime] = number_coins
-    amount -= coins[:dime] * number_coins
-  end
-  if amount >= coins[:nickel]
-    change[:nickel] = 1
-    amount -= coins[:nickel]
-  end
-  if amount >= 1
-    change[:penny] = amount
-  end
+
   change
 end
