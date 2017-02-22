1e34960b7b1941818a03213e9a5616e8bde616ce
jverbosky <jverbosk@gmail.com>
2017-02-22 12:03:25 -0500

    replaced coin values with variable names

--- in_class/coin_changer/coin_changer.rb
+++ in_class/coin_changer/coin_changer.rb
@@ -1,18 +1,22 @@
 def get_change(amount)
   change = {}
-  if amount >= 25
-    number_coins = amount / 25
+  quarter = 25
+  dime = 10
+  nickel = 5
+  penny = 1
+  if amount >= quarter
+    number_coins = amount / quarter
     change[:quarter] = number_coins
-    amount -= 25 * number_coins
+    amount -= quarter * number_coins
   end
-  if amount >= 10
-    number_coins = amount / 10
+  if amount >= dime
+    number_coins = amount / dime
     change[:dime] = number_coins
-    amount -= 10 * number_coins
+    amount -= dime * number_coins
   end
-  if amount >= 5
+  if amount >= nickel
     change[:nickel] = 1
-    amount -= 5
+    amount -= nickel
   end
   if amount >= 1
     change[:penny] = amount
