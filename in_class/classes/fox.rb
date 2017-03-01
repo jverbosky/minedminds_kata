2fde09888db64c5c2e27ab9bd3ac37c8d812811c
jverbosky <jverbosk@gmail.com>
2017-03-01 14:17:56 -0500

    added dog and fox classes and files, tests for dog and fox done and passing

--- in_class/classes/fox.rb
+++ in_class/classes/fox.rb
@@ -1,4 +1,4 @@
-class Cat
+class Fox
 
   # Method to get the name back (getters & setters in other languages)
   attr_reader :name, :sound
@@ -6,7 +6,7 @@ class Cat
   def initialize(name)
     # use instance variable if not resetting the value anywhere else
     @name = name
-    @sound = "meow"
+    @sound = "konkon"
   end
 
 end
--- in_class/classes/test_animal_sounds.rb
+++ in_class/classes/test_animal_sounds.rb
@@ -1,5 +1,7 @@
 require "minitest/autorun"
 require_relative "cat.rb"
+require_relative "dog.rb"
+require_relative "fox.rb"
 
 class TestAnimalSounds < Minitest::Test
 
@@ -11,6 +13,26 @@ class TestAnimalSounds < Minitest::Test
   def test_2_test_for_cat_sound
     animal = Cat.new("Midnight")
     assert_equal("meow", animal.sound)
+  end
+
+  def test_3_test_for_dog_name
+    animal = Dog.new("Tan-chan")
+    assert_equal("Tan-chan", animal.name)
+  end
+
+  def test_4_test_for_dog_sound
+    animal = Dog.new("Luma")
+    assert_equal("woof", animal.sound)
+  end
+
+    def test_5_test_for_fox_name
+    animal = Fox.new("Red")
+    assert_equal("Red", animal.name)
+  end
+
+  def test_6_test_for_fox_sound
+    animal = Fox.new("Todd")
+    assert_equal("konkon", animal.sound)
   end
 
 end
